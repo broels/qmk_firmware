d09c82f1278209af6f99f05b8de704a76ead45ad
Brian Roels <broels@salsify.com>
2019-03-08 09:30:17 -0500

    This seems to work even better

--- keyboards/kbdfans/kbd6x/keymaps/broels/config.h
+++ keyboards/kbdfans/kbd6x/keymaps/broels/config.h
@@ -1,7 +1,10 @@
 #include "../../config.h"
 
 #undef TAPPING_TERM
-#define TAPPING_TERM 250
+#define TAPPING_TERM 200
+
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 2
 
 #define PERMISSIVE_HOLD
 /* #define IGNORE_MOD_TAP_INTERRUPT */
