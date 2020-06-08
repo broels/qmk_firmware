594630312037c98a0a2b54d41a04163db4a32174
Brian Roels <broels@salsify.com>
2020-06-08 10:38:56 -0400

    dusting off the tofu

--- keyboards/kbdfans/kbd6x/keymaps/broels/config.h
+++ keyboards/kbdfans/kbd6x/keymaps/broels/config.h
@@ -1,12 +1,14 @@
 #include "../../config.h"
 
-#define FORCE_NKRO
+/* #define FORCE_NKRO */
 
-#undef TAPPING_TERM
-#define TAPPING_TERM 200
+/* #undef TAPPING_TERM */
+/* #define TAPPING_TERM 200 */
 
-#undef TAPPING_TOGGLE
-#define TAPPING_TOGGLE 2
+/* #undef TAPPING_TOGGLE */
+/* #define TAPPING_TOGGLE 2 */
 
 #define PERMISSIVE_HOLD
 /* #define IGNORE_MOD_TAP_INTERRUPT */
+
+#define ONESHOT_TAP_TOGGLE 2
