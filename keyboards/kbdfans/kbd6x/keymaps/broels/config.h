bb5d478fb73aaf905e2fb5b117db360bd5370180
Brian Roels <broels@salsify.com>
2020-06-08 14:38:35 -0400

    fixes

--- keyboards/kbdfans/kbd6x/keymaps/broels/config.h
+++ keyboards/kbdfans/kbd6x/keymaps/broels/config.h
@@ -1,14 +1,15 @@
 #include "../../config.h"
 
-/* #define FORCE_NKRO */
+#define FORCE_NKRO
 
-/* #undef TAPPING_TERM */
-/* #define TAPPING_TERM 200 */
+#undef TAPPING_TERM
+#define TAPPING_TERM 200
 
-/* #undef TAPPING_TOGGLE */
-/* #define TAPPING_TOGGLE 2 */
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 2
 
 #define PERMISSIVE_HOLD
-/* #define IGNORE_MOD_TAP_INTERRUPT */
+#define IGNORE_MOD_TAP_INTERRUPT
 
+#undef ONESHOT_TAP_TOGGLE
 #define ONESHOT_TAP_TOGGLE 2
